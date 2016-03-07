1ea9e61875d8e59614f887112c69a5b63aaf064b
Gopal Saini <gopal.saini.work@gmail.com>
2016-03-07 22:18:38 +0530

    Added more test cases

--- test/specs/jasmine.spec.js
+++ test/specs/jasmine.spec.js
@@ -5,4 +5,12 @@ describe('addition', function () {
     it('should return 4 when adding 2 and 2', function () {
         expect(2 + 2).toBe(4);
     });
+    
+    it('should return 6 when adding 4 and 2', function () {
+        expect(4 + 2).toBe(6);
+    });
+    
+    it('should return 8 when adding 6 and 2', function () {
+        expect(6 + 2).toBe(8);
+    });
 });
